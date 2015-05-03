a54aed1969a149ef76ec429c10d8970203c5af52
Ralph <schaef@inf.fu-berlin.de>
2015-05-03 18:21:53 +0200

    Add copyright note to unit test source files.

--- src/test/java/test/java/AllTests.java
+++ src/test/java/test/java/AllTests.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * This file is part of the Corporate Smart Content Project at Freie Universitaet Berlin, Corporate Semantic Web Group.
+ * 
+ * This work has been partially supported by the "InnoProfile-Corporate Semantic Web" project funded by the German Federal Ministry of Education and Research (BMBF) and the BMBF Innovation Initiative for the New German Laender - Entrepreneurial Regions.
+ * 
+ * http://www.corporate-smart-content.de/
+ * 
+ * Freie Universitaet Berlin
+ * Copyright (c) 2013-2016
+ * 
+ * Institut fuer Informatik
+ * Working Group Corporate Semantic Web
+ * Koenigin-Luise-Strasse 24-26
+ * 14195 Berlin
+ * 
+ * http://www.mi.fu-berlin.de/en/inf/groups/ag-csw/
+ ******************************************************************************/
 package test.java;
 
 import org.junit.runner.RunWith;
